ecc41b58216eb4b95320761ae8bea3887e0b3194
Igor Postolenko <i.postolenko@scalablesolutions.io>
2024-05-15 14:32:48 +0300

    portfolio currency filter

--- src/app/portfolio/Portfolio.tsx
+++ src/app/portfolio/Portfolio.tsx
@@ -1,15 +1,28 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Table, TableColumn } from '../../shared/ui';
 import { Position } from './page';
+import { Select } from '@mantine/core';
+import { uniqBy } from 'lodash';
 
 interface Props {
   data: Position[];
 }
 
+// TODO: filter by broker
+
 const Portfolio: React.FC<Props> = props => {
-  const assets = props.data;
+  const [currency, setCurrency] = useState<string | null>(null);
+
+  const currencyOptions = uniqBy(props.data, item => item.asset.currency).map(item => ({
+    value: item.asset.currency!,
+    label: item.asset.currency!,
+  }));
+
+  const assets = currency
+    ? props.data.filter(item => item.asset.currency === currency)
+    : props.data;
 
   const columns: TableColumn<Position>[] = [
     { name: 'asset', type: 'object', getPresentation: data => data.asset.name },
@@ -19,7 +32,13 @@ const Portfolio: React.FC<Props> = props => {
     { name: 'price', type: 'number', width: 150, precision: 2 },
     { name: 'priceDelta', title: 'Price (%)', type: 'number', width: 150, precision: 2 },
     { name: 'quantity', type: 'number', width: 150 },
-    { name: 'amount', type: 'number', width: 150, precision: 0 },
+    {
+      name: 'amount',
+      type: 'number',
+      width: 150,
+      precision: 0,
+      footer: !!currency,
+    },
     {
       name: 'amountInRub',
       title: 'Amount (RUB)',
@@ -31,7 +50,22 @@ const Portfolio: React.FC<Props> = props => {
   ];
 
   return (
-    <Table data={assets} columns={columns} initialSort={[{ field: 'weight', order: 'desc' }]} />
+    <>
+      <div style={{ display: 'flex' }}>
+        <div style={{ width: 200 }}>
+          <Select
+            label={'Currency'}
+            searchable
+            clearable
+            value={currency}
+            onChange={setCurrency}
+            data={currencyOptions}
+          />
+        </div>
+      </div>
+      <br />
+      <Table data={assets} columns={columns} initialSort={[{ field: 'weight', order: 'desc' }]} />
+    </>
   );
 };
 
